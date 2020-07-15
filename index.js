aee69dcf460a38734d078677befcad88b15cef9c
unknown <ashwinlaly@gmail.com>
2020-07-15 22:35:06 +0530

    Response changes

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var express = require('express')
 var app = express()
 
 app.get("/", (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.send('Hello World!');
 })
 
 app.listen(3000, () => {
