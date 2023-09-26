638a0146ab2e5c0d0c3c3340ca6d6702833c7e7e
Ashwin <ashwinlaly@gmail.com>
2023-09-26 13:35:34 +0530

    socket

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var http = require('http');
 var express = require('express')
 var app = express()
+const { Server } = require('socket.io');
 
-app.get("/", (req, res) => {
-  res.send('Hello World!');
-})
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(process.env.PORT, () => {
-  console.log(1)
-})
+// Define a Socket.IO event handler for incoming connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Define event handlers for custom events
+  socket.on('chat message', (message) => {
+    console.log('Received message:', message);
+    
+    // Broadcast the message to all connected clients
+    io.emit('chat message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
